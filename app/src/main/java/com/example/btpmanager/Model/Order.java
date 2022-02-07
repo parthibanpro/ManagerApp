71af841295521e70c9736c6379721797b259378b
parthiban <parthiban5259@gmail.com>
2022-02-07 15:07:41 +0800

    Manager App

---
+++ app/src/main/java/com/example/btpmanager/Model/Order.java
@@ -0,0 +1,73 @@
+package com.example.btpmanager.Model;
+
+import java.util.ArrayList;
+
+public class Order {
+
+    private String customerName, customerNumber, customerID, customerImage, amountPaid, timestamp,orderStatus, docId, custOrderDocID, payment,tableNo;
+    private ArrayList<FoodQty> ordersList;
+
+    public Order(String customerName, String customerNumber, String customerID, String customerImage, String amountPaid, String timestamp, String orderStatus, ArrayList<FoodQty> ordersList, String docId, String custOrderDocID, String payment, String tableNo) {
+        this.customerName = customerName;
+        this.customerNumber = customerNumber;
+        this.customerID = customerID;
+        this.customerImage = customerImage;
+        this.amountPaid = amountPaid;
+        this.timestamp = timestamp;
+        this.orderStatus = orderStatus;
+        this.ordersList = ordersList;
+        this.custOrderDocID = custOrderDocID;
+        this.payment = payment;
+        this.tableNo = tableNo;
+
+        this.docId = docId;
+    }
+
+    public String getTableNo() {
+        return tableNo;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public String getCustOrderDocID() {
+        return custOrderDocID;
+    }
+
+    public String getDocId() {
+        return docId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public String getCustomerImage() {
+        return customerImage;
+    }
+
+    public String getAmountPaid() {
+        return amountPaid;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public ArrayList<FoodQty> getOrdersList() {
+        return ordersList;
+    }
+}
